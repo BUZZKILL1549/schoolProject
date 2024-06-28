d9485be9c818974effdfa4ce6ade1d1cb87b81c2
BUZZKILL1549 <aushvs10@hotmail.com>
2024-06-28 21:40:13 +0530

    added primary key name

--- initializeDatabase.sql
+++ initializeDatabase.sql
@@ -2,32 +2,40 @@ CREATE DATABASE school_app;
 
 USE school_app;
 
+CREATE TABLE users(
+    ID                  INT         NOT NULL,
+    Uname               VARCHAR(50) NOT NULL,
+    Pwd                 VARCHAR(50) NOT NULL,
+
+    CONSTRAINT PK_ID    PRIMARY KEY (ID)
+);
+
 CREATE TABLE announcements(
     Title               VARCHAR(50) NOT NULL,
     Description         TEXT        NOT NULL,
 
-    PRIMARY KEY (Title)
+    CONSTRAINT PK_Title PRIMARY KEY (Title)
 );
 
 CREATE TABLE assignments(
     Title               VARCHAR(50) NOT NULL,
     Description         TEXT        NOT NULL,
 
-    PRIMARY KEY (Title)
+    CONSTRAINT PK_Title PRIMARY KEY (Title)
 );
 
 CREATE TABLE class_diary(
     Title               VARCHAR(50) NOT NULL,
     Description         TEXT        NOT NULL,
 
-    PRIMARY KEY (Title)
+    CONSTRAINT PK_Title PRIMARY KEY (Title)
 );
 
 CREATE TABLE parent_concern(
     Subject             VARCHAR(50) NOT NULL,
     Description         TEXT        NOT NULL,
 
-    PRIMARY KEY (Subject)
+    CONSTRAINT PK_Title PRIMARY KEY (Subject)
 );
 
 
