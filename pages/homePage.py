7e6ca1811f085d43f91a1bcc2d05f530bf204168
BUZZKILL1549 <aushvs10@hotmail.com>
2024-07-01 20:16:04 +0530

    added auto-update of data in treeview

--- pages/homePage.py
+++ pages/homePage.py
@@ -1,62 +1,33 @@
 from tkinter import *
 from tkinter import ttk
-
 import components.announcementsPage as announcementsPage
 import components.assignmentsPage as assignmentsPage
 import components.classDiaryPage as classDiaryPage
 import components.parentConcernPage as parentConcernPage
 
-def home():
-    homePage = Toplevel()
+def home(root):
+    homePage = Toplevel(root)
     homePage.title("Home Page")
 
     # Appbar area
     appBarFrame = ttk.Frame(homePage)
-    appBarFrame.grid(padx = 10, pady = 10)
-    ttk.Label(appBarFrame, text = "BVM GLOBAL").grid(row = 0, column = 0)
+    appBarFrame.grid(padx=10, pady=10)
+    ttk.Label(appBarFrame, text="BVM GLOBAL").grid(row=0, column=0)
 
     homeFrame = ttk.Frame(homePage)
-    homeFrame.grid(padx = 10, pady = 10)
+    homeFrame.grid(padx=10, pady=10)
 
     # Rest of the app
-    announcementButton = ttk.Button(homeFrame, text = "Announcements", command = announcementsPage.announcements)
-    announcementButton.grid(row = 1, column = 0, padx = 10, pady = 20)
+    announcementButton = ttk.Button(homeFrame, text="Announcements", command=lambda: announcementsPage.announcements(root))
+    announcementButton.grid(row=1, column=0, padx=10, pady=20)
 
-    assignmentsButton = ttk.Button(homeFrame, text = "Assignments", command = assignmentsPage.assignments)
-    assignmentsButton.grid(row = 1, column = 2, padx = 10, pady = 20)
+    assignmentsButton = ttk.Button(homeFrame, text="Assignments", command=lambda: assignmentsPage.assignments(root))
+    assignmentsButton.grid(row=1, column=2, padx=10, pady=20)
 
-    classDiaryButton = ttk.Button(homeFrame, text = "Class Diary", command = classDiaryPage.classDiary)
-    classDiaryButton.grid(row = 1, column = 1, padx = 10, pady = 20)
+    classDiaryButton = ttk.Button(homeFrame, text="Class Diary", command=lambda: classDiaryPage.classDiary(root))
+    classDiaryButton.grid(row=1, column=1, padx=10, pady=20)
 
-    parentConcernButton = ttk.Button(homeFrame, text = "Parent Concern", command = parentConcernPage.parentConcern)
-    parentConcernButton.grid(row = 2, column = 1, padx = 10, pady = 30, sticky = E)
-
-    homePage.mainloop()
-
-if __name__ == "__main__":
-    homePage = Tk()
-    homePage.title("Home Page")
-
-    # Appbar area
-    appBarFrame = ttk.Frame(homePage)
-    appBarFrame.grid(padx = 10, pady = 10)
-    ttk.Label(appBarFrame, text = "BVM GLOBAL").grid(row = 0, column = 0)
-
-    homeFrame = ttk.Frame(homePage)
-    homeFrame.grid(padx = 10, pady = 10)
-
-    # Rest of the app
-    announcementButton = ttk.Button(homeFrame, text = "Announcements", command = announcementsPage.announcements)
-    announcementButton.grid(row = 1, column = 0, padx = 10, pady = 20)
-
-    assignmentsButton = ttk.Button(homeFrame, text = "Assignments", command = assignmentsPage.assignments)
-    assignmentsButton.grid(row = 1, column = 2, padx = 10, pady = 20)
-
-    classDiaryButton = ttk.Button(homeFrame, text = "Class Diary", command = classDiaryPage.classDiary)
-    classDiaryButton.grid(row = 1, column = 1, padx = 10, pady = 20)
-
-    parentConcernButton = ttk.Button(homeFrame, text = "Parent Concern", command = parentConcernPage.parentConcern)
-    parentConcernButton.grid(row = 2, column = 1, padx = 10, pady = 30, sticky = E)
+    parentConcernButton = ttk.Button(homeFrame, text="Parent Concern", command=lambda: parentConcernPage.parentConcern(root))
+    parentConcernButton.grid(row=2, column=1, padx=10, pady=30, sticky=E)
 
     homePage.mainloop()
-
